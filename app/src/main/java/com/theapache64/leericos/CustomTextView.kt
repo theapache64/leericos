48fcd04bbafffad533079207f2fe1dcf03593589
theapache64 <theapache64@gmail.com>
2019-04-16 12:48:46 +0530

    🌟 Added circle animation to music tag

--- app/src/main/java/com/theapache64/leericos/CustomTextView.kt
+++ app/src/main/java/com/theapache64/leericos/CustomTextView.kt
@@ -1,9 +1,35 @@
 package com.theapache64.leericos
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 
 class CustomTextView(context: Context?, attrs: AttributeSet?) : AppCompatTextView(context, attrs) {
 
+    private val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.parseColor("#F8F8F8")
+        style = Paint.Style.FILL
+    }
+
+    var radius = 0f
+    override fun onDraw(c: Canvas?) {
+        if (text == "(music)") {
+            c?.let { canvas ->
+                val cx = width / 2.toFloat()
+                val cy = height / 2.toFloat()
+                val maxRad = cx
+                if (radius > cx) {
+                    radius = 0f
+                } else {
+                    radius += 10
+                }
+
+                canvas.drawCircle(cx, cy, radius, circlePaint)
+            }
+        }
+        super.onDraw(c)
+    }
 }
